acbe3364a27d47eab06866169055d53a4d92a8f8
sookoo19 <kouhei20001011@gmail.com>
2025-11-18 22:31:16 +0100

    Intro is made

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode, Suspense, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Loading from './components/Loading';
-import Intro from './pages/Intro'
+import Intro from './pages/Intro';
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
